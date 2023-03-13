f6b06ab27bd48bdcbcfef6ba1d44c95e0e980ac1
Nazar <nazariykharchuk@gmail.com>
2023-03-13 10:21:04 +0200

    Changed the task #1 of laboratory work #2

--- Lab2/Task1.js
+++ Lab2/Task1.js
@@ -1,30 +1,143 @@
-"use strict";
+window.onload = function () {
+  // отримання об'єкта canvas
+  let canvas = document.getElementById("my-canvas");
 
-//Отримуємо координати прямокутника канвасу
-let canvas = document.getElementById("my-canvas");
-let context = canvas.getContext("2d");
+  // отримання контексту WebGL
+  let gl = WebGLUtils.setupWebGL(canvas);
 
-//Масив, що містить кольори для точок
-let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+  if (!gl) {
+    alert("WebGL не підтримується вашим браузером!");
+    return;
+  }
 
-//Отримуємо координати прямокутника канвасу
-let rect = canvas.getBoundingClientRect();
+  // налаштування viewport
+  gl.viewport(0, 0, canvas.width, canvas.height);
 
-//Функція, що створює точку на місці курсора
-function createPoint(event) {
-  //Виправляємо позицію курсора в межах прямокутника канвасу
-  let x = event.clientX - rect.left;
-  let y = event.clientY - rect.top;
+  // встановлення кольору екрану
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-  //Рандомно вибираємо колір точки
-  let color = colors[Math.floor(Math.random() * colors.length)];
+  // очищення екрану
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //Створюємо точку
-  context.fillStyle = color;
-  context.beginPath();
-  context.arc(x, y, 10, 0, 2 * Math.PI);
-  context.fill();
-}
+  // створення вершинного шейдера
+  let vertexShaderSource = `
+          attribute vec3 aVertexPosition;
+          attribute vec3 aVertexColor;
+          varying vec3 vColor;
+          void main() {
+              gl_Position = vec4(aVertexPosition, 1.0);
+              gl_PointSize = 10.0;
+              vColor = aVertexColor;
+          }
+      `;
 
-//Додаємо обробник подій на натискання миші
-canvas.addEventListener("mousedown", createPoint);
+  let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderSource);
+  gl.compileShader(vertexShader);
+
+  // перевірка успішності компіляції вершинного шейдера
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error(
+      "Помилка компіляції вершинного шейдера:",
+      gl.getShaderInfoLog(vertexShader)
+    );
+    return;
+  }
+
+  // створення фрагментного шейдера
+  let fragmentShaderSource = `
+          precision mediump float;
+          varying vec3 vColor;
+          void main() {
+              gl_FragColor = vec4(vColor, 1.0);
+          }
+      `;
+
+  let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderSource);
+  gl.compileShader(fragmentShader);
+
+  // перевірка успішності компіляції фрагментного шейдера
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error(
+      "Помилка компіляції фрагментного шейдера:",
+      gl.getShaderInfoLog(fragmentShader)
+    );
+    return;
+  }
+
+  // створення програми шейдерів та прив'язка шейдерів до програми
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  // перевірка успішності створення програми шейдерів
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(
+      "Помилка створення програми шейдерів:",
+      gl.getProgramInfoLog(program)
+    );
+    return;
+  }
+
+  // використання програми шейдерів
+  gl.useProgram(program);
+
+  // створення буфера вершин
+  let vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+  // встановлення покажчиків на атрибути
+  let positionPointer = gl.getAttribLocation(program, "aVertexPosition");
+  gl.enableVertexAttribArray(positionPointer);
+  gl.vertexAttribPointer(
+    positionPointer,
+    3,
+    gl.FLOAT,
+    false,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+
+  let colorPointer = gl.getAttribLocation(program, "aVertexColor");
+  gl.enableVertexAttribArray(colorPointer);
+  gl.vertexAttribPointer(
+    colorPointer,
+    3,
+    gl.FLOAT,
+    false,
+    6 * Float32Array.BYTES_PER_ELEMENT,
+    3 * Float32Array.BYTES_PER_ELEMENT
+  );
+
+  // отримуємо координати прямокутника канвасу
+  let rect = canvas.getBoundingClientRect();
+
+  // змінна, що буде зберігати координати та кольори точок
+  let points = [];
+
+  // функція, що створює точку на місці курсора
+  function createPoint(event) {
+    // виправляємо позицію курсора в межах прямокутника канвасу
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    // додаємо точку до буферу
+    points.push(
+      (x / canvas.width) * 2 - 1,
+      1 - (y / canvas.height) * 2,
+      0.0,
+      Math.random(),
+      Math.random(),
+      Math.random()
+    );
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+
+    // малюємо точки
+    gl.drawArrays(gl.POINTS, 0, points.length / 6);
+  }
+
+  // додаємо обробник подій на натискання миші
+  canvas.addEventListener("mousedown", createPoint);
+};
